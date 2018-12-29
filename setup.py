0f742a9ca9cd8cf439468115bcb388b76ceb8d53
timorl <tymorl@gmail.com>
2018-12-29 13:08:55 +0100

    Added dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 
 import sys
 import distutils.core
-import mannequin
 
 if sys.version_info[0] < 3:
     print("Mannequin requires Python 3")
@@ -9,10 +8,20 @@ if sys.version_info[0] < 3:
 
 distutils.core.setup(
     name="mannequin",
-    version=".".join(map(str, mannequin.version)),
+    version="2.7.2",
     description="An ultra-compact reinforcement learning framework",
     author="SquirrelInHell",
     author_email="squirrelinhell@users.noreply.github.com",
     url="https://github.com/squirrelinhell/mannequin2/",
     packages=["mannequin"],
+    install_requires=[
+        "gym",
+        "numpy",
+        "matplotlib",
+        "autograd",
+        "tensorflow",
+        "ipython",
+        "box2d-py",
+        "baselines",
+        ],
 )
